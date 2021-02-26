9c7e8429201e0c5eeec106d0702f8009a854ea61
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-26 17:31:55 -0500

    added to todo, stuck debuugging end of phase 3

--- vanilla_js/src/todo_list.js
+++ vanilla_js/src/todo_list.js
@@ -1,10 +1,35 @@
 let toDo = [];
 const ul = document.getElementsByClassName("todos")[0];
 const form = document.getElementsByClassName("add-todo-form")[0];
-
+debugger
 export const addToDo = function () {
-  form.addEventListener("submit", () => {
-    const input = document.getElementsByName("add-todo")[0];
-    const inputValue = input.value();
-  });
+    form.addEventListener("submit", (e) => {
+        debugger
+        e.preventDefault();
+        const input = document.getElementsByName("add-todo")[0];
+        const inputValue = input.value();
+        let toDoObj = {
+            value: inputValue,
+            done: false,
+        }
+        toDo.push(toDoObj);
+        input.value("");
+
+        populateList(toDo);
+    });
 };
+//[obj, obj, obj3]
+const li = document.createElement("li");
+
+export const populateList = function (toDo) {
+    (toDo).forEach( obj => {
+        let label = document.createElement("label");
+        label.innerHTML = toDo[obj]["value"];
+        let checked = document.createElement("input");
+        checked.setAttribute("type", "checkbox");
+        let toDoLi = document.createElement("li");
+        toDoLi.append(checked);
+        toDoLi.append(label);
+        ul.append(toDoLi);
+    })
+}
