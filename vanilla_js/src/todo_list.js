9ebb39b4d691a812673bd6a712f2908ec9ba35f3
App Academy Student <lxg073000@gmail.com>
2021-02-26 16:53:29 -0500

    sneakycommit

--- vanilla_js/src/todo_list.js
+++ vanilla_js/src/todo_list.js
@@ -1,2 +1,10 @@
+let toDo = [];
+const ul = document.getElementsByClassName("todos")[0];
+const form = document.getElementsByClassName("add-todo-form")[0];
 
-
+export const addToDo = function () {
+  form.addEventListener("submit", () => {
+    const input = document.getElementsByName("add-todo")[0];
+    const inputValue = input.value();
+  });
+};
